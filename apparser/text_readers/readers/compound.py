a3b23608c60a2b916586c9c7ac854b157b986b77
lexter0705 <lexter0705@gmail.com>
2026-06-06 17:12:47 +0300

    1) Added CraftDetector class to apparser.text_readers.detectors 2) Added TrocrScanner class to apparser.text_readers.scanners 3) Added realization to apparser.text_readers.CompoundReader class

--- apparser/text_readers/readers/compound.py
+++ apparser/text_readers/readers/compound.py
@@ -1,10 +1,36 @@
 import numpy
 
-from apparser.text_readers.readers.base import BaseTextReader
-from apparser.text_readers.models import TextData
+from apparser.geometry import QuadPoints, distance
 
+from apparser.text_readers.readers.base import BaseTextReader
+
+from apparser.text_readers.models import TextData
 from apparser.text_readers.detectors import BaseTextDetector
 from apparser.text_readers.scanners import BaseTextScanner
+
+
+import numpy
+from PIL import Image
+
+
+def _cut_by_coordinates(image: numpy.ndarray, coordinates: QuadPoints) -> numpy.ndarray:
+    pil_image = Image.fromarray(image)
+    if pil_image.mode not in ("RGB", "RGBA", "L"):
+        pil_image = pil_image.convert("RGB")
+    left_top = (coordinates.left_top.x, coordinates.left_top.y)
+    right_top = (coordinates.right_top.x, coordinates.right_top.y)
+    right_bottom = (coordinates.right_bottom.x, coordinates.right_bottom.y)
+    left_bottom = (coordinates.left_bottom.x, coordinates.left_bottom.y)
+    out_width = int(max(distance(left_top, right_top), distance(left_bottom, right_bottom)))
+    out_height = int(max(distance(left_top, left_bottom), distance(right_top, right_bottom)))
+    out_size = (max(out_width, 1), max(out_height, 1))
+    quad_data = (left_top[0], left_top[1],
+                 right_top[0], right_top[1],
+                 right_bottom[0], right_bottom[1],
+                 left_bottom[0], left_bottom[1])
+    transformed = pil_image.transform(out_size, Image.QUAD, quad_data,
+                                      resample=Image.BICUBIC)
+    return numpy.array(transformed)
 
 
 class CompoundReader(BaseTextReader):
@@ -36,4 +62,8 @@ class CompoundReader(BaseTextReader):
         :return: Detected text data.
         :rtype: list[TextData]
         """
-        pass
+        result = []
+        for coordinates in self.__detector.read_image(image):
+            cuted_image = _cut_by_coordinates(image, coordinates)
+            text = self.__scanner.read_image(cuted_image)
+            result.append(TextData(text=text, coordinates=coordinates))
